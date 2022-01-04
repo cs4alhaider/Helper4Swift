4e182370a67286d8171a78015649ab0e0399cd83
cs4alhaider <cs.alhaider@gmail.com>
2022-01-04 15:11:16 +0300

    Added Binding extension

---
+++ Sources/Helper4Swift/Extensions/Binding+Extension.swift
@@ -0,0 +1,28 @@
+//
+//  Binding+Extension.swift
+//  Helper4Swift
+//
+//  Created by Abdullah Alhaider on 04/01/2022.
+//
+
+import SwiftUI
+
+@available(iOS 13.0, *)
+extension Binding {
+    
+    /// Get notified when the value changed
+    /// 
+    /// - Parameter handler: your own logic
+    ///
+    /// - Returns: same value
+    func onChange(_ handler: @escaping (Value) -> Void) -> Binding<Value> {
+        Binding(
+            get: { self.wrappedValue },
+            set: { newValue in
+                self.wrappedValue = newValue
+                handler(newValue)
+            }
+        )
+    }
+}
+
