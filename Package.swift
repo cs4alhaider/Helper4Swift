2a93e792ed756c60f6b28dd33807d09db4a408e6
cs4alhaider <cs.alhaider@gmail.com>
2022-01-09 01:17:49 +0300

    update

--- Package.swift
+++ Package.swift
@@ -5,12 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "Helper4Swift",
-    platforms: [
-        .iOS(SupportedPlatform.IOSVersion.v10),
-        .tvOS(SupportedPlatform.TVOSVersion.v10),
-        .watchOS(SupportedPlatform.WatchOSVersion.v3),
-        .macOS(SupportedPlatform.MacOSVersion.v10_13)
-    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
