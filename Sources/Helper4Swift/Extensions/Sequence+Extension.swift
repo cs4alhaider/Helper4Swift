9c7fb4401615765833da77cdad676ae5d85f030a
Abdullah Alhaider <cs.alhaider@gmail.com>
2022-03-19 17:42:07 +0300

    Added Sequence extension

---
+++ Sources/Helper4Swift/Extensions/Sequence+Extension.swift
@@ -0,0 +1,24 @@
+//
+//  Sequence+Extension.swift
+//  Helper4Swift
+//
+//  Created by Abdullah Alhaider on 19/03/2022.
+//
+
+import Foundation
+
+enum SortOrder {
+    case increasing, decreasing
+}
+
+extension Sequence {
+    
+    func sorted<Value: Comparable>(by keyPath: KeyPath<Self.Element, Value>, order: SortOrder = .increasing) -> [Self.Element] {
+        switch order {
+        case .increasing:
+            return self.sorted(by: { $0[keyPath: keyPath]  <  $1[keyPath: keyPath] })
+        case .decreasing:
+            return self.sorted(by: { $0[keyPath: keyPath]  >  $1[keyPath: keyPath] })
+        }
+    }
+}
