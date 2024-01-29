d6b9f31f7db1b19b6f2d71efc08c0a2a8b397063
Abdullah Alhaider <cs.alhaider@gmail.com>
2024-01-29 14:56:23 +0300

    Added isDarkColor for Color extension

--- Sources/Helper4Swift/Extensions/Color+Extension.swift
+++ Sources/Helper4Swift/Extensions/Color+Extension.swift
@@ -9,23 +9,23 @@ import SwiftUI
 
 @available(iOS 13.0, *)
 public extension Color {
- 
+    
     func uiColor() -> UIColor {
-
+        
         if #available(iOS 14.0, *) {
             return UIColor(self)
         }
-
+        
         let components = self.components()
         return UIColor(red: components.r, green: components.g, blue: components.b, alpha: components.a)
     }
-
+    
     private func components() -> (r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat) {
-
+        
         let scanner = Scanner(string: self.description.trimmingCharacters(in: CharacterSet.alphanumerics.inverted))
         var hexNumber: UInt64 = 0
         var r: CGFloat = 0.0, g: CGFloat = 0.0, b: CGFloat = 0.0, a: CGFloat = 0.0
-
+        
         let result = scanner.scanHexInt64(&hexNumber)
         if result {
             r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
@@ -45,34 +45,68 @@ public extension Color {
         let r: CGFloat = components?[0] ?? 0
         let g: CGFloat = components?[1] ?? 0
         let b: CGFloat = components?[2] ?? 0
-
-        return String(format: "#%02lX%02lX%02lX", 
-                      lroundf(Float(r * 255)), 
-                      lroundf(Float(g * 255)), 
+        
+        return String(format: "#%02lX%02lX%02lX",
+                      lroundf(Float(r * 255)),
+                      lroundf(Float(g * 255)),
                       lroundf(Float(b * 255)))
     }
-
+    
     // Initialize Color from Hex String
     init(hex: String) {
         let hexString = hex.trimmingCharacters(in: .whitespacesAndNewlines)
         let scanner = Scanner(string: hexString)
-
+        
         if hexString.hasPrefix("#") {
             scanner.currentIndex = hexString.index(after: hexString.startIndex)
         }
-
+        
         var color: UInt64 = 0
         scanner.scanHexInt64(&color)
-
+        
         let mask = 0x000000FF
         let r = Int(color >> 16) & mask
         let g = Int(color >> 8) & mask
         let b = Int(color) & mask
-
+        
         let red   = CGFloat(r) / 255
         let green = CGFloat(g) / 255
         let blue  = CGFloat(b) / 255
-
+        
         self.init(red: red, green: green, blue: blue)
+    }
+    
+    
+    /// Determines if the color is perceived as dark or light.
+    /// This method calculates the relative luminance of the color in the sRGB space
+    /// and compares it against a threshold to decide if the color is dark.
+    ///
+    /// - Returns: A Boolean value indicating whether the color is dark (`true`) or light (`false`).
+    func isDarkColor() -> Bool {
+        // Extract RGB components from the color
+        guard let components = UIColor(self).cgColor.components, components.count >= 3 else {
+            return false
+        }
+        
+        // Assign RGB components to respective variables
+        let red = components[0]
+        let green = components[1]
+        let blue = components[2]
+        
+        // Calculate luminance for each color component.
+        // The formula converts the linear RGB values to sRGB,
+        // which better represents human perception of brightness.
+        let redLuminance = red < 0.03928 ? red / 12.92 : pow((red + 0.055) / 1.055, 2.4)
+        let greenLuminance = green < 0.03928 ? green / 12.92 : pow((green + 0.055) / 1.055, 2.4)
+        let blueLuminance = blue < 0.03928 ? blue / 12.92 : pow((blue + 0.055) / 1.055, 2.4)
+        
+        // Calculate the overall luminance of the color using the standard coefficients.
+        // These coefficients (0.2126, 0.7152, 0.0722) correspond to the human eye's
+        // sensitivity to red, green, and blue light, respectively.
+        let luminance = 0.2126 * redLuminance + 0.7152 * greenLuminance + 0.0722 * blueLuminance
+        
+        // Compare the luminance against a threshold to determine if the color is dark.
+        // The threshold value of 0.5 can be adjusted based on specific requirements.
+        return luminance < 0.5
     }
 }
