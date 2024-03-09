0b8c305dddbc7486c5e056416008c1aa18e6cf73
Abdullah Alhaider <cs.alhaider@gmail.com>
2024-03-10 00:42:42 +0300

    Update Binding+Extension.swift

--- Sources/Helper4Swift/Extensions/Binding+Extension.swift
+++ Sources/Helper4Swift/Extensions/Binding+Extension.swift
@@ -26,3 +26,11 @@ public extension Binding {
     }
 }
 
+@available(iOS 13.0, *)
+public func ??<T>(lhs: Binding<Optional<T>>, rhs: T) -> Binding<T> {
+    Binding(
+        get: { lhs.wrappedValue ?? rhs },
+        set: { lhs.wrappedValue = $0 }
+    )
+}
+
