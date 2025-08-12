8fdf2226625f0f7fcb51f3fed5515e2eea5df027
cs4alhaider <cs.alhaider@gmail.com>
2025-08-12 21:57:22 +0300

    Remove Equatable conformance from CLLocationCoordinate2D extension, simplifying the codebase.

--- Sources/Helper4Swift/Extensions/CLLocationCoordinate2D+Extension.swift
+++ Sources/Helper4Swift/Extensions/CLLocationCoordinate2D+Extension.swift
@@ -12,9 +12,3 @@ public extension CLLocationCoordinate2D {
         .init(latitude: latitude, longitude: longitude)
     }
 }
-
-extension CLLocationCoordinate2D: Equatable {
-    public static func == (lhs: CLLocationCoordinate2D, rhs: CLLocationCoordinate2D) -> Bool {
-        lhs.latitude == rhs.latitude && lhs.longitude == rhs.longitude
-    }
-}
